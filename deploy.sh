1883b82fd86e4bc9a9866be915cb169f59a3c6b4
minxd95 <newobj95@gmail.com>
2022-11-08 15:56:39 +0900

    1232142142

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 APP_NAME=retry-server
 
 # 패키지 재설치
+cd /home/ubuntu/drcloud-deploy
 rm -rf ./node_modules
 npm ci --only=production
 
 # pm2 describe의 exit code가 0이 아니면 (앱이 실행중이지 않으면)
 # 앱을 실행하고, 1 이면 (실행중이면) 앱을 재실행한다.
-pm2 describe retry-server > /dev/null
+pm2 describe $APP_NAME > /dev/null
 RUNNING=$?
 
 if [ ${RUNNING} -ne 0 ]; then
-  pm2 start ./dist/main.js --name retry-server
+  pm2 start ./dist/main.js --name $APP_NAME
 else
   pm2 restart $APP_NAME
 fi
