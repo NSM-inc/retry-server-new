6c88dddddd5fe4c84a3a8fe897df3a17d3ff2b03
minxd95 <newobj95@gmail.com>
2022-11-08 17:13:58 +0900

    cluster mode

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,12 @@ rm -rf ./node_modules
 npm ci --only=production
 
 # pm2 describe의 exit code가 0이 아니면 (앱이 실행중이지 않으면)
-# 앱을 실행하고, 1 이면 (실행중이면) 앱을 재실행한다.
+# 앱을 실행하고, 0 이면 (실행중이면) 앱을 재실행한다.
 pm2 describe $APP_NAME > /dev/null
 RUNNING=$?
 
 if [ ${RUNNING} -ne 0 ]; then
-  pm2 start ./dist/main.js --name $APP_NAME
+  npm run start:prod
 else
-  pm2 restart $APP_NAME
+  npm run reload:prod
 fi
